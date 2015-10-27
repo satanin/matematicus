01342bb80379b1b72f2ccd4c93f42973e1891ee0
Raul Garcia <satanin@gmail.com>
2015-10-27 17:56:36 +0100

    Added login with facebook

---
+++ app/controllers/devise/confirmations_controller.rb
@@ -0,0 +1,28 @@
+class Devise::ConfirmationsController < DeviseController
+
+ # GET /resource/confirmation?confirmation_token=abcdef
+  def show
+    self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+    yield resource if block_given?
+
+    if resource.errors.empty?
+      set_flash_message(:notice, :confirmed) if is_flashing_format?
+      sign_in(resource)
+      respond_with_navigational(resource){ redirect_to after_confirmation_path_for(resource_name, resource) }
+    else
+      respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
+    end
+  end
+
+  protected
+
+  # The path used after confirmation.
+  def after_confirmation_path_for(resource_name, resource)
+    if signed_in?(resource_name)
+      user_path(resource)
+      #signed_in_root_path(resource)
+    else
+      new_session_path(resource_name)
+    end
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,16 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable, :omniauth_providers => [:facebook]
 
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
+
+  def self.from_omniauth(auth)
+    user = User.find_by(email: auth.info.email)
+    user = User.create(email: auth.info.email, password: Devise.friendly_token[0,20], username: auth.info.name, confirmation_token: nil, confirmed_at: Time.now.utc, confirmation_sent_at: Time.now.utc) if user.nil?
+    user
+  end
+
 end
