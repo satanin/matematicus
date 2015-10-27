9990d53779c9acb9f4c73ad0607c3edfea6e4c25
Raul Garcia <satanin@gmail.com>
2015-10-27 13:00:49 +0100

    added tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @user = User.new(email: "user@email.com", password: "12345678", username: "test1")
+  end
+
+  it "cannot be creted without username" do
+    @user.username = nil
+    expect(@user.save).to be(false)
+  end
+
+  it "cannot be creted without email" do
+    @user.email = nil
+    expect(@user.save).to be(false)
+  end
+
+  it "cannot be creted without password" do
+    @user.password = nil
+    expect(@user.save).to be(false)
+  end
+
+  it "Two users cannot have same username" do
+    second_user = @user.dup
+    second_user.email = "another@email.com"
+    @user.save
+    expect(second_user.save).to be(false)
+  end
+
+  it "Two users cannot have same email" do
+    second_user = @user.dup
+    second_user.username = "test2"
+    @user.save
+    expect(second_user.save).to be(false)
+  end
+
+  it "email must have correct formatting" do
+    @user.email = "ho@@la que ase@email malo . com "
+    expect(@user.save).to be(false)
+  end
+
+  it "can be created with email, password and username" do
+    user = User.new(email: "user@email.com", password: "12345678", username: "test1")
+
+    expect(@user.save).to be(true)
+  end
+
+  context "password" do
+    it "cannot be smaller than 8 chars" do
+      @user.password = "1234567"
+      expect(@user.save).to be(false)
+    end
+  end
 end
