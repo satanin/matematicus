873f45b2e75e538e7b0f5be9bc49d274c8c00117
Raul Garcia <satanin@gmail.com>
2015-10-30 16:31:50 +0100

    Added edit to user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,26 @@ class UsersController < ApplicationController
   def show
     @user = current_user
   end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    result = @user.update_attributes(user_params)
+
+    if result
+      redirect_to  user_path(@user), notice: "#{t(:updated, scope: :users)}"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email)
+  end
 end
