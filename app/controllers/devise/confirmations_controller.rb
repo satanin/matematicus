f8950429f329fccc2f869ca7a116757b0821d869
Raul Garcia <satanin@gmail.com>
2015-10-31 01:31:27 +0100

    Fix devise registration form

--- app/controllers/devise/confirmations_controller.rb
+++ app/controllers/devise/confirmations_controller.rb
@@ -20,7 +20,6 @@ class Devise::ConfirmationsController < DeviseController
   def after_confirmation_path_for(resource_name, resource)
     if signed_in?(resource_name)
       user_path(resource)
-      #signed_in_root_path(resource)
     else
       new_session_path(resource_name)
     end
