709914497658721d497d3e7368a5a82e988e69fd
Raul Garcia <satanin@gmail.com>
2015-11-02 19:18:28 +0100

    Added some format and small fixes

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,5 @@
 class AnswersController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit, :update]
   before_action :answer_for_question, :set_user
   before_action :set_answer, only: [:edit, :update]
 
