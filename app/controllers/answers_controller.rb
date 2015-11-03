bc9102aa5358079033cb3f7c53ae19a7a8bfc886
Raul Garcia <satanin@gmail.com>
2015-11-03 18:16:28 +0100

    Added user must be logged to answer

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,5 +1,5 @@
 class AnswersController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :edit, :update, :create]
   before_action :answer_for_question, :set_user
   before_action :set_answer, only: [:edit, :update]
 
