700880ff76d020e1d17ef61e15edb9dd052b0abf
Sergio Revilla <replymusic@gmail.com>
2015-11-08 13:49:49 +0100

    Updated seed data

--- db/seeds/production.rb
+++ db/seeds/production.rb
@@ -8,6 +8,15 @@ User.create!(
   )
 
 User.create!(
+  username: "elreplicante",
+  email: "replymusic@gmail.com",
+  password: "12345678",
+  confirmation_token: nil,
+  confirmed_at: Time.now.utc,
+  confirmation_sent_at: Time.now.utc
+  )
+
+User.create!(
   username: "John Doe",
   email: "johndoe@test.com",
   password: "12345678",
@@ -26,16 +35,11 @@ User.create!(
   )
 
 Question.create!(
-  title: "This is question one",
+  title: "Matrices y determinantes",
   body: "This is question's one body",
   user_id: 1
   )
 
-QuestionComment.create!(
-  body: "This is a comment for Question one.",
-  question_id: 1,
-  user_id: 2
-  )
 
 Answer.create!(
   body: "Answer's body",
@@ -43,8 +47,15 @@ Answer.create!(
   user_id: 2
   )
 
-AnswerComment.create!(
-  body: "This is a comment for Answer",
-  answer_id: 1,
+Question.create!(
+  title: "Matrices y determinantes",
+  body: "This is question's one body",
   user_id: 1
+  )
+
+
+Answer.create!(
+  body: "La respuesta",
+  question_id: 1,
+  user_id: 2
   )
