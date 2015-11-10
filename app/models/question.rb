c489cfbca5a98e97c124be099579017ababd2245
Raul Garcia <satanin@gmail.com>
2015-11-10 03:59:22 +0100

    Add user must add at least one tag to question

--- app/models/question.rb
+++ app/models/question.rb
@@ -7,6 +7,7 @@ class Question < ActiveRecord::Base
   validates :user_id, presence: true
   validates :title, presence: true, length: { maximum: 300 }
   validates :body, presence: true
+  validates :tags, presence: true
 
 
   def increase_views
