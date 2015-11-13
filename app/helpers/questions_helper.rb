7a12828848cf5f83a081b1cd703a386fb27ff09d
Raul Garcia <satanin@gmail.com>
2015-11-13 18:31:21 +0100

    Added some style to views

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -2,4 +2,18 @@ module QuestionsHelper
   def outside_question_view
     /\/questions\/[0-9]/.match(request.env['PATH_INFO']).nil?
   end
+
+  def edit_question_toggle current_user, question  
+    link_to "<i class='fa fa-pencil'></i>".html_safe, edit_question_path(question) if user_can_edit_question? current_user,question
+  end
+
+  def user_can_edit_question? current_user,question
+    question.user == current_user
+  end
+
+  def user_can_vote? current_user,question
+    user_vote = Vote.find_by(user_id: current_user.id, question_id: question.id)
+    puts "*"*20, user_vote.nil?
+    user_vote.nil?
+  end
 end
