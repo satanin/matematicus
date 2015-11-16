2aa0e2dcc5b25b7ef3cab14917c25e4908897c1a
ZeroLive <zerolive@gmail.com>
2015-11-16 16:11:18 +0100

    Added test for vote model

--- spec/factories/question.rb
+++ spec/factories/question.rb
@@ -25,5 +25,14 @@ FactoryGirl.define do
         create_list(:vote, evaluator.votes_count, question: question)
       end
     end
+    factory :question_with_answers do
+      transient do
+        answers_count 5
+      end
+
+      after(:create) do |question, evaluator|
+        create_list(:answer, evaluator.answers_count, question: question)
+      end
+    end
   end
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe Vote, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	let(:user){create(:user)}
+	let(:question){create(:question_with_answers)}
+	let(:answer){create(:answer)}
+
+	it "user can vote a question" do
+		votes = Vote.all.count
+		question_votes = question.votes.count
+		Vote.create(user_id: user.id, question_id: question.id, value: 1)
+
+		expect(Vote.all.count).to eq votes+1
+		expect(question.votes.count).to eq question_votes+1
+	end
+
+	it "user can vote a answer" do
+		votes = Vote.all.count
+		answer_votes = answer.votes.count
+		Vote.create(user_id: user.id, answer_id: answer.id, value: 1)
+
+		expect(Vote.all.count).to eq votes+1
+		expect(answer.votes.count).to eq answer_votes+1
+	end
+
+	it "dont let users vote twice the same question" do
+		vote_one = Vote.create(question_id: question.id, user_id: user.id, value: 1)
+		vote_two = Vote.create(question_id: question.id, user_id: user.id, value: 1)
+
+		expect(vote_one.errors.count).to eq 0
+		expect(vote_two.errors.count).to be > 0
+	end
+
+	it "dont let users vote twice the same answer" do
+		vote_one = Vote.create(answer_id: answer.id, user_id: user.id, value: 1)
+		vote_two = Vote.create(answer_id: answer.id, user_id: user.id, value: 1)
+
+		expect(vote_one.errors.count).to eq 0
+		expect(vote_two.errors.count).to be > 0
+	end
+
 end
