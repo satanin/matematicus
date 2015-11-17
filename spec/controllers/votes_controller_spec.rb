ea583236217ecad51e50483ad2cbd3864c3710ee
ZeroLive <zerolive@gmail.com>
2015-11-17 12:39:50 +0100

    Added test to vote controller, vote up and down for answer

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe VotesController, type: :controller do
   let(:question) { create(:question) }
+  let(:answer) { create(:answer) }
 
   context "when user is not logged in" do
 
@@ -9,6 +10,33 @@ RSpec.describe VotesController, type: :controller do
 
       it "redirects to log in" do
         xhr :get, :vote_up_question, question_id: question.id, format: :js
+
+        expect(response.status).to be 401
+      end
+    end
+
+    describe "GET #vote_down_question" do
+
+      it "redirects to log in" do
+        xhr :get, :vote_down_question, question_id: question.id, format: :js
+
+        expect(response.status).to be 401
+      end
+    end
+
+    describe "GET #vote_up_answer" do
+
+      it "redirects to log in" do
+        xhr :get, :vote_up_answer, answer_id: answer.id, format: :js
+
+        expect(response.status).to be 401
+      end
+    end
+
+    describe "GET #vote_up_answer" do
+
+      it "redirects to log in" do
+        xhr :get, :vote_up_answer, answer_id: answer.id, format: :js
 
         expect(response.status).to be 401
       end
@@ -43,6 +71,28 @@ RSpec.describe VotesController, type: :controller do
         expect(response.status).to eq 200
         expect(question.votes_value).to eq "-1"
         expect(response).to render_template("update_question_votes")
+      end
+    end
+
+    describe "GET #vote_up_answer" do
+
+      it "decreases vote value by 1" do
+        xhr :get, :vote_up_answer, answer_id: answer.id, format: :js
+
+        expect(response.status).to eq 200
+        expect(answer.votes_value).to eq "1"
+        expect(response).to render_template("update_answer_votes")
+      end
+    end
+
+    describe "GET #vote_down_answer" do
+
+      it "decreases vote value by 1" do
+        xhr :get, :vote_down_answer, answer_id: answer.id, format: :js
+
+        expect(response.status).to eq 200
+        expect(answer.votes_value).to eq "-1"
+        expect(response).to render_template("update_answer_votes")
       end
     end
   end
