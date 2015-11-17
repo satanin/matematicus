931856280a0e976c3aa4636621de8bec407ea684
Raul Garcia <satanin@gmail.com>
2015-11-17 10:48:55 +0100

    Fix questions can be seen by not logged visitors

--- app/helpers/answers_helper.rb
+++ app/helpers/answers_helper.rb
@@ -7,7 +7,7 @@ module AnswersHelper
   end
 
   def user_can_vote answer
-    user_vote = Vote.find_by(user_id: current_user.id, answer_id: answer.id)
+    user_vote = Vote.find_by(user_id: current_user.id, answer_id: answer.id) if current_user
 
     html = content_tag(:div, class:"large-1 columns vote-controls", id:"answer#{answer.id}-vote-controls") do
         concat "<i class='fa fa-chevron-up'></i>".html_safe
