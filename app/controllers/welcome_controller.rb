dfc0d96df8bd06742026281d5e6d9d770c566f96
ZeroLive <zerolive@gmail.com>
2015-12-21 17:42:12 +0100

    Added panels to
    right side on welcome index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,8 @@ class WelcomeController < ApplicationController
 
   def index
     @questions = Question.order(updated_at: :desc)
+    @most_viewed_questions = Question.order(times_viewed: :desc).limit(5)
+    @most_voted_answers = Answer.all.sort_by{|answer| answer.votes_value }.reverse[0..4]
   end
 
 end
